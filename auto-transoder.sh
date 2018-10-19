36f9adf96718d823a73fced72ee0d74873a007ae
Damian96 <damiang23ftr@gmail.com>
2018-10-19 17:23:05 +0300

    Improvements

--- auto-transoder.sh
+++ auto-transoder.sh
@@ -3,42 +3,55 @@
 # Set Debugging
 # set -x;
 
-echo "Please insert the video's URL: ";
-read -r URL;
+URL="";
 
-if [[ -z $URL ]]; then
-	echo "Error: Empty URL provided";
-else
-	while [ true ]; do
+getURL() {
+	echo "Please insert the video's URL: ";
+	read -r URL;
 
-		echo "What do you want to do?";
-		echo "1. View available formats of media.";
-		echo "2. Download media.";
-		echo "3. Download & Transcode media to highest quality audio.";
-		echo "['q': exit]";
-		echo ": ";
-		read -r OPTION;
+	if [[ -z $URL ]]; then
+		echo "Error: Empty URL provided";
+		exit 1;
+	else
+		echo "$URL";
+	fi;
+}
 
-		if [[ "$OPTION" == "q" ]]; then
-			exit;
-		elif [[ "$OPTION" == "1" ]]; then
-			youtube-dl "$URL" -F;
-		elif [[ "$OPTION" == "2" ]]; then
-			echo "Please insert the media's format id: ";
-			read -r FORMAT;
-			youtube-dl "$URL" -f "$FORMAT";
-		elif [[ "$OPTION" == "3" ]]; then
-			ID=`youtube-dl "$URL" --get-id`;
-			TITLE=`youtube-dl "$URL" --get-title`;
-			FILE="./$ID.webm";
-			TARGET="$TITLE-$ID.ogg";
+while [ true ]; do
 
-			youtube-dl "$URL" -f 171 --id -q;
-			ffmpeg -i "$FILE" -vn -b:a 128k -vol 270 -codec libvorbis "$TARGET";
-			rm "$FILE";
+	echo "                ###########							 ";
+	echo "What do you want to do?";
+	echo "1. View available formats of media.";
+	echo "2. Download media.";
+	echo "3. Download & Transcode media to highest quality audio.";
+	echo "['q': exit]";
+	echo ": ";
+	read -r OPTION;
 
-			echo "Audio ready @:\"./$TARGET\"";
-		fi;
+	if [[ "$OPTION" == "q" ]]; then
+		echo "Bye!";
+		exit;
+	elif [[ "$OPTION" == "1" ]]; then
+		getURL;
+		youtube-dl "$URL" -F;
+	elif [[ "$OPTION" == "2" ]]; then
+		getURL;
+		echo "Please insert the media's format id: ";
+		read -r FORMAT;
+		youtube-dl "$URL" -f "$FORMAT";
+	elif [[ "$OPTION" == "3" ]]; then
+		getURL;
+		ID=`youtube-dl "$URL" --get-id`;
+		TITLE=`youtube-dl "$URL" --get-title`;
+		FILE="./$ID.webm";
+		TARGET="$TITLE-$ID.ogg";
 
-	done
-fi;
+		youtube-dl "$URL" -f 171 --id -q;
+		ffmpeg -i "$FILE" -vn -b:a 128k -vol 270 -codec libvorbis "$TARGET";
+		rm "$FILE";
+
+		echo "      ############		 ";
+		echo "Audio ready @:\"./$TARGET\"";
+		echo "      ############		 ";
+	fi;
+done
